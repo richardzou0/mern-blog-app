6e6d22693513c1e0df653da86e23fff05eed5703
Richard Zou <zouric@bc.edu>
2025-05-13 17:53:23 +0000

    Enable CORS and update server config

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 const Post = require('./models/Post');
