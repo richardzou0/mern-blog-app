a2cd81c807195404ddcc0a7d05901f3ac5a60ae4
Richard Zou <zouric@bc.edu>
2025-05-13 17:54:16 +0000

    Add tags field to Post model with max limit validation

--- models/Post.js
+++ models/Post.js
@@ -2,6 +2,20 @@ const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
   title: { type: String, required: true },
+  body: { type: String, required: true },
+  tags: {
+    type: [String],
+    default: [],
+    validate: [arrayLimit, '{PATH} exceeds the limit of 5']
+  }
+}, { timestamps: true });
+
+function arrayLimit(val) {
+  return val.length <= 5;
+}
+
+module.exports = mongoose.model('Post', postSchema);onst postSchema = new mongoose.Schema({
+  title: { type: String, required: true },
   body: { type: String, required: true }
 }, { timestamps: true });
 
